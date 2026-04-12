18950a234055688b634257054ef3333773ac6021
julianrodelo <julianrodelo@gmail.com>
2026-04-12 17:10:31 -0500

    fix: eliminate external protoc dependency using protobuf-src and version bump to 0.1.21

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Dile a tonic-build que use el compilador de Protobuf interno (compilado de fuente)
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+    
     tonic_build::compile_protos("proto/bittice.proto")?;
     Ok(())
 }
